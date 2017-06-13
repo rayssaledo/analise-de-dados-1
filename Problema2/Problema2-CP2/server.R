dc360e8c5865b835cb5d53e876b0091ddb66315d
Rayssa <rayssaledo@gmail.com>
2017-06-12 22:18:50 -0300

    Adicionando os gráficos linha e a seleção de séries

--- Problema2/Problema2-CP2/server.R
+++ Problema2/Problema2-CP2/server.R
@@ -12,30 +12,54 @@ library("dplyr")
 library("tidyr")
 library("ggplot2")
 library("readr")
+library(plotly)
 
 
 dados = read_csv("../../dados/series_from_imdb.csv")
 
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  # choose columns to display
-  series_name2 = dados %>% select(series_name) %>%  distinct()
-  series_name2[sample(nrow(dados %>% select(series_name)), 31), ]
-  output$mytable1 <- DT::renderDataTable({
-    DT::datatable(series_name2[, input$show_vars, drop = FALSE])
+  
+  output$distPlot <- renderPlotly({
+    
+    dados <- dados %>% filter(series_name == "Sense8")
+    
+    plot = dados %>%  
+            ggplot(aes(x = series_ep, y = UserRating)) +
+            geom_line() +
+            geom_point(aes(text = paste("Episódio:", series_ep, "<br>", "Avaliação:", UserRating)),
+                       color = "purple") +
+            labs(title = "Avaliação dos usuários por episódio da série 'Sense8'", x = "Episódios", y = "Avaliações dos usuários")
+
+          ggplotly(plot, tooltip = "text")
   })
   
-  # # sorted columns are colored now because CSS are attached to them
-  # output$mytable2 <- DT::renderDataTable({
-  #   DT::datatable(mtcars, options = list(orderClasses = TRUE))
-  # })
-  # 
-  # # customize the length drop-down menu; display 5 rows per page by default
-  # output$mytable3 <- DT::renderDataTable({
-  #   DT::datatable(iris, options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
-  # })
+  output$distPlot2 <- renderPlotly({
+    dados <- dados %>% filter(series_name %in% input$series)
   
+    if(input$series != '') {
+      p = dados %>%
+        ggplot(aes(x = series_ep, y = UserRating)) +
+        geom_line() +
+        geom_point(aes(text = paste("Episódio:", series_ep, "<br>", "Classificação:", UserRating)),
+                   color = "orange",
+                   size = 2) +
+        scale_x_continuous(breaks=seq(1, 25, 5))+
+        facet_wrap(~series_name, scales = "free_x") +
+        labs(title = "Avaliação dos usuários por episódio da série: ", x = "Episódios", y = "Avaliações dos usuários") %>% return()
+        
+      ggplotly(p, tooltip = "text")
+    } else if (input$series == '') {
+      dados %>%
+        ggplot(aes(x = UserRating)) + 
+        geom_histogram(binwidth = .5, fill = "pink", color = "black") + 
+        geom_rug() +
+        labs(title = "Avaliação dos usuários por episódio da série: ", x = "Episódios", y = "Avaliações dos usuários") %>% return()
+    }
+  
+  })
+
 })
 
 
--- Problema2/Problema2-CP2/ui.R
+++ Problema2/Problema2-CP2/ui.R
@@ -9,27 +9,39 @@
 
 library(shiny)
 library(ggplot2)
+library(plotly)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Séries"),
+  titlePanel("Comparando a série que possui mais avaliações positivas com as demais"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      checkboxGroupInput('show_vars', 'Colunas:',
-                         names(dados %>% select(series_name)), selected = names(dados %>% select(series_name)))
+      selectInput('series', "Escolha uma série para comparar com 'Sense8': ", 
+                  choices = list(Choose='', 
+                                 "13 Reasons Why" = "13 Reasons Why",
+                                 "Black Mirror" = "Black Mirror",
+                                 "How to Get Away with Murder" = "How to Get Away with Murder",
+                                 "Mr Robot" = "Mr Robot",
+                                 "Sherlock" = "Sherlock",
+                                 "Breaking Bad"="Breaking Bad",
+                                 "Dexter"="Dexter",
+                                 "House of Cards"="House of Cards",
+                                 "Stranger Things"="Stranger Things"), 
+                  selected = 1)
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      tabsetPanel(
-        id = 'dataset',
-        tabPanel('series_name', DT::dataTableOutput('mytable1'))
-        # tabPanel('mtcars', DT::dataTableOutput('mytable2')),
-        # tabPanel('iris', DT::dataTableOutput('mytable3'))
+      # plotlyOutput("distPlot")
+      fluidRow(
+        column(12,plotlyOutput("distPlot"))
+      ),
+      fluidRow(
+        column(12,plotlyOutput("distPlot2"))
       )
     )
   )
